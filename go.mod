723e4c81c388ce8ae6bc4a9567d3b86060ca9d4e
freesinger <wzy545@vip.qq.com>
2022-03-23 18:59:23 +0800

    add download_test

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.2.2
 	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6
 	gopkg.in/gcfg.v1 v1.2.3
 	gopkg.in/warnings.v0 v0.1.2 // indirect
--- parser/download_test.go
+++ parser/download_test.go
@@ -1,1 +1,46 @@
 package parser
+
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDownloadAndExtract(t *testing.T) {
+	outDir := "../output"
+	url := "https://www.elastic.co/guide/en/elasticsearch/reference/current/index.html"
+	resp, err := http.Get(url)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+	err = Download(url, outDir, resp.Body)
+	assert.NoError(t, err)
+	_, err = Extract(resp)
+	assert.NoError(t, err)
+	assert.NoError(t, os.Remove(filepath.Join(outDir, format(url))))
+}
+
+func TestFormat(t *testing.T) {
+	cases := []struct {
+		in  string
+		out string
+	}{
+		{
+			in:  "http://www.baidu.com",
+			out: "www.baidu.com",
+		},
+		{
+			in:  "https://cloud.baidu.com/BFE",
+			out: "cloud.baidu.com_BFE",
+		},
+		{
+			in:  "http://family.com/help_index.html",
+			out: "family.com_help-index.html",
+		},
+	}
+	for i := range cases {
+		assert.Equal(t, cases[i].out, format(cases[i].in))
+	}
+}
